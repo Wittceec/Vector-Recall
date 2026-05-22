7bbce54347ec2925228c3bbc71c141dab5ff6e4c
Christopher Wittman <wittceec@Christophers-MacBook.local>
2026-05-22 18:02:16 -0400

    Explicitly install onnxruntime-node to ensure Vercel includes the binary dependencies in deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['onnxruntime-node'],
+  serverExternalPackages: ['onnxruntime-node', 'sharp'],
 };
 
 export default nextConfig;
